b02b251b4ad676e2ae9aa5b126b566d0e5eeaf82
Minino99 <loizzo.pasqualino@gmail.com>
2022-05-20 15:20:56 +0200

    tutte le milestone complete + vari fixes

--- js/script.js
+++ js/script.js
@@ -2,12 +2,16 @@ const resultBoxHtml = document.querySelector(".row");
 
 for (let i = 1; i <= 100; i++) {
   if (i % 3 === 0 && i % 5 === 0) {
+   console.log("FizzBuzz")
     resultBoxHtml.innerHTML += `<div class="div col square fizzbuzzbg">fizzbuzz</div>`;
   } else if (i % 3 === 0) {
+   console.log("Fizz")
     resultBoxHtml.innerHTML += `<div class="div col square fizzbg">fizz</div>`;
   } else if (i % 5 === 0) {
+   console.log("Buzz")
     resultBoxHtml.innerHTML += `<div class="div col square buzzbg">buzz</div>`;
   } else {
+   console.log(i)
     resultBoxHtml.innerHTML += `<div class="div col square">${i}</div>`;
   }
 }
